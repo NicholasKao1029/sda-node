2e318a5f43d8b3bf4f9980411c90b42cb41189fc
NicholasKao1029 <kobenkao@gmail.com>
2023-02-11 10:23:29 -0800

    remove dependency on tensorrt repo

--- inf-setup.sh
+++ inf-setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+# assumes python3.10 is installed
+# assumes env var TRT_OSSPATH is set to the path of the TensorRT OSS repo
+
 python3.10 -m venv venv && 
 source venv/bin/activate && 
-pip install -r ../TensorRT/demo/Diffusion/requirements.txt && # TODO: move requirements from this repo to here
+pip install -r tensorrt-requirements &&
 pip install -r requirements.txt && 
 pip install --upgrade pip && 
 pip install --upgrade tensorrt && 
