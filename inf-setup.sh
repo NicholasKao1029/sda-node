9e90a4951a81a81539e2b23f91c25d8a5bc7efd6
NicholasKao1029 <kobenkao@gmail.com>
2023-02-12 13:36:07 -0800

    lfg

--- inf-setup.sh
+++ inf-setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # assumes python3.10 is installed
+# 3.10.8 works
+# 3.10.5 TBD
 # assumes env var TRT_OSSPATH is set to the path of the TensorRT OSS repo
 
-python3.10 -m venv venv && 
+python -m venv venv && 
 source venv/bin/activate && 
 pip install -r tensorrt-requirements.txt &&
 pip install -r requirements.txt && 
 pip install --upgrade pip && 
 pip install --upgrade tensorrt && 
+pip install nvidia-pyindex
+pip install polygraphy
+pip install transformers
+pip install cuda-python
+pip install scipy
 pip install gunicorn
