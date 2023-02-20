78d26509031a93ab567b04f3c879375028e4f70a
Nick Kao <kobenkao@gmail.com>
2023-02-20 13:57:35 -0800

    cmoipoer setup

--- compiler-setup.sh
+++ compiler-setup.sh
@@ -1,7 +1,7 @@
 python3.10 -m venv compiler-venv && 
 source compiler-venv/bin/activate &&
-pip install -r sda-node/requirements.txt &&
-pip install -r TensorRT/demo/Diffusion/requirements.txt &&
+pip install -r requirements.txt &&
+pip install -r tensorrt-requirements.txt &&
 pip install --upgrade pip && 
 pip install --upgrade tensorrt && 
 pip install diffusers==0.7.2
