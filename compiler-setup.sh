bfb9c735f0f809fbdfbde330de1f76704ccfe079
Nick Kao <kobenkao@gmail.com>
2023-02-25 10:49:47 -0800

    compiler setup

--- compiler-setup.sh
+++ compiler-setup.sh
@@ -1,9 +1,9 @@
-python3.10 -m venv compiler-venv && 
-source compiler-venv/bin/activate &&
-pip install -r requirements.txt &&
-pip install -r tensorrt-requirements.txt &&
-pip install --upgrade pip && 
-pip install --upgrade tensorrt && 
+python3.10 -m venv compiler-venv  
+source compiler-venv/bin/activate
+pip install -r requirements.txt 
+pip install -r tensorrt-requirements.txt 
+pip install --upgrade pip  
+pip install --upgrade tensorrt  
 pip install diffusers==0.7.2
 pip install nvidia-pyindex
 pip install polygraphy
